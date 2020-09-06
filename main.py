c0313588525d6cb9a0d6fb2e2e5ea15984c4caa4
wli4 <70674123+wli4@users.noreply.github.com>
2020-09-06 16:24:41 -0400

    update

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import sys
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+number = 5
+guess = input("Guess a number from 1 to 10: ")
+
+if guess not in ['1' ,'2','3', '4', '5', '6', '7', '8', '9', '10']:
+    raise ValueError("Please limit the guess to a number from 1 to 10.")
+
+guess = int(guess)
+if guess == number:
+    print("Great job! You got it!")
+else:
+    print("Sorry, better luck next time.")
+    print("The number was " + str(number))
